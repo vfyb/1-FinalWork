8a9adaaf9e7ac053632d2d8f7ab1770bd4e4f87b
Alex Karnakov <ak2005@yandex.ru>
2022-10-22 21:40:12 +0300

    Make code more accurate

--- Program.cs
+++ Program.cs
@@ -33,7 +33,7 @@ void PrintArrayToConsole(string[] array)
     Console.Write("[ ");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i != array.Length -1) Console.Write($"{array[i]}, ");
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
         else Console.Write($"{array[i]} ");
     }
     Console.WriteLine("]");
